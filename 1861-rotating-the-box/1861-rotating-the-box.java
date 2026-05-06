cc3286f178ead892de2cb3bac0c1a60f926d2b51
Tanu chouhan <rajput.tanu3113@gmail.com>
2026-05-07 00:19:35 +0530

    Time: 284 ms (5.05%), Space: 124.7 MB (60.34%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -0,0 +1,32 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        
+        for (int i = 0; i < m; i++) {
+            int emptySpot = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '#') {
+                   
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][emptySpot] = '#';
+                    emptySpot--;
+                } else if (boxGrid[i][j] == '*') {
+                   
+                    emptySpot = j - 1;
+                }
+            }
+        }
+
+       
+        char[][] rotatedBox = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedBox[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return rotatedBox;
+    }
+}
