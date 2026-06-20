acf613c5157ab28f5d9c9e7a5668d4b8b100d7fc
Tanu chouhan <rajput.tanu3113@gmail.com>
2026-06-20 23:15:47 +0530

    Time: 55 ms (73.53%), Space: 125.6 MB (26.47%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.java
+++ 1840-maximum-building-height/1840-maximum-building-height.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxBuilding(int n, int[][] restrictions) {
         int m = restrictions.length;
@@ -9,12 +7,11 @@ class Solution {
         r[1] = new int[]{n, n - 1};
         
         System.arraycopy(restrictions, 0, r, 2, m);
-        
+     
         Arrays.sort(r, (a, b) -> Integer.compare(a[0], b[0]));
         
         int len = r.length;
-        
-     
+      
         for (int i = 1; i < len; i++) {
             int dist = r[i][0] - r[i - 1][0];
             r[i][1] = Math.min(r[i][1], r[i - 1][1] + dist);
@@ -31,7 +28,7 @@ class Solution {
             int h1 = r[i - 1][1];
             int id2 = r[i][0];
             int h2 = r[i][1];
-         
+        
             int peak = (h1 + h2 + (id2 - id1)) / 2;
             maxOverallHeight = Math.max(maxOverallHeight, peak);
         }
