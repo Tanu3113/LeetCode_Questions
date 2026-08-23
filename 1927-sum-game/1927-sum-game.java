0b92a79b0eebf4e61038dfe9719db48459f7b02e
Tanu chouhan <rajput.tanu3113@gmail.com>
2026-08-23 17:19:27 +0530

    Time: 7 ms (79.56%), Space: 46.6 MB (94.89%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int sumDiff = 0;
+        int countDiff = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            char c = num.charAt(i);
+            if (c == '?') {
+                countDiff++;
+            } else {
+                sumDiff += (c - '0');
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            char c = num.charAt(i);
+            if (c == '?') {
+                countDiff--;
+            } else {
+                sumDiff -= (c - '0');
+            }
+        }
+
+        return (countDiff % 2 != 0) || (2 * sumDiff + countDiff * 9 != 0);
+    }
+}
