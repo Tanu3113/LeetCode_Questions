447c7d500d826f13c13c2c1397de5e1c3e1bc2ae
Tanu chouhan <rajput.tanu3113@gmail.com>
2026-09-02 20:22:41 +0530

    Time: 28 ms (78.82%), Space: 136.4 MB (90.99%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,61 +1,38 @@
-import java.util.*;
-
 class Solution {
     public int[] validSequence(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
 
-        
-        int[] last = new int[m];
-        int ptr = n - 1;
+        int[] last = new int[m + 1];
+        last[m] = n;
 
+        int p = n - 1;
         for (int j = m - 1; j >= 0; j--) {
-            while (ptr >= 0 && word1.charAt(ptr) != word2.charAt(j)) {
-                ptr--;
+            while (p >= 0 && word1.charAt(p) != word2.charAt(j)) {
+                p--;
             }
-            last[j] = ptr;
-            if (ptr >= 0) {
-                ptr--; 
+            last[j] = p;
+            if (p >= 0) {
+                p--;
             }
         }
 
         int[] result = new int[m];
-        boolean changed = false;
-        int i = 0; 
+        int j = 0;
+        boolean mismatchUsed = false;
 
-        for (int j = 0; j < m; j++) {
-            boolean matched = false;
-
-            while (i < n) {
-                boolean isCharMatch = word1.charAt(i) == word2.charAt(j);
-
-                if (isCharMatch) {
-                    result[j] = i;
-                    i++;
-                    matched = true;
-                    break;
-                } else if (!changed) {
-                  
-                    boolean canCompleteSuffix = (j == m - 1) || (last[j + 1] > i);
-
-                    if (canCompleteSuffix) {
-                        result[j] = i;
-                        changed = true;
-                        i++;
-                        matched = true;
-                        break;
-                    }
-                }
-
-                i++;
-            }
-
+        for (int i = 0; i < n && j < m; i++) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                result[j] = i;
+                j++;
+            } else if (!mismatchUsed && last[j + 1] > i) {
             
-            if (!matched) {
-                return new int[0];
+                result[j] = i;
+                mismatchUsed = true;
+                j++;
             }
         }
 
-        return result;
+        return j == m ? result : new int[0];
     }
 }
